09f6265c9c90806fac17cbf2eeb876fee48663be
FlorianGrollich <F-Grollich@web.de>
2023-03-25 01:29:13 +0100

    add all properties for match model

--- match.py
+++ match.py
@@ -2,5 +2,23 @@
 
 class Match:
     def __init__(self):
-        self.team1 = []
-        self.team2 = []
+        self.startingTeam1 = []
+        self.benchTeam1 = []
+        self.coachTeam1 = ""
+        self.endScoreTeam1 = 0
+        self.halfScoreTeam1 = 0
+        self.teamId1 = ""
+
+
+        self.startingTeam2 = []
+        self.benchTeam2= []
+        self.coachTeam2 = ""
+        self.endScoreTeam2 = 0
+        self.halfScoreTeam2 = 0
+        self.teamId2 = ""
+
+        self.ref = ""
+        self.stadium = ""
+        self.date = ""
+        self.tempreture = ""
+        self.competition = ""
