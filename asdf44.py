b69ef5636a9cf68bcc1c53ed586f79eed2e8736f
FlorianGrollich <F-Grollich@web.de>
2023-09-04 22:05:28 +0200

    add user agent and first spider

--- asdf44.py
+++
@@ -1,9 +0,0 @@
-x = "asdf"
-print(x.split(""))
-for cha in resultWord:
-    try:
-        index = searchWord[lastIndex::].index(cha)
-        lastIndex = index
-        sub_string += cha
-    except ValueError:
-        break
--- transfermarkt/transfermarkt/spiders/transfermarktspider.py
+++ transfermarkt/transfermarkt/spiders/transfermarktspider.py
@@ -4,7 +4,14 @@ import scrapy
 class TransfermarktspiderSpider(scrapy.Spider):
     name = "transfermarktspider"
     allowed_domains = ["transfermarkt.de"]
-    start_urls = ["https://transfermarkt.de"]
+    start_urls = ["https://www.transfermarkt.de/spielbericht/index/spielbericht/4095985"]
 
     def parse(self, response):
-        pass
+        spieler = response.css('span.aufstellung-rueckennummer-name')
+
+
+        for player in spieler:
+            yield {
+                'name': player.css('a::text').get(),
+            }
+
