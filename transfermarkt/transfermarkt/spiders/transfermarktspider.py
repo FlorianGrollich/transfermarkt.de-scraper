55651327ae7489ea69b7fc66ced0235729b39d42
FlorianGrollich <F-Grollich@web.de>
2023-12-20 23:34:36 +0100

    feat: add utils

--- transfermarkt/transfermarkt/spiders/transfermarktspider.py
+++ transfermarkt/transfermarkt/spiders/transfermarktspider.py
@@ -1,7 +1,7 @@
 import scrapy
 
 
-class TransfermarktspiderSpider(scrapy.Spider):
+class TransfermarktsSpider(scrapy.Spider):
     name = "transfermarktspider"
     allowed_domains = ["transfermarkt.de"]
     start_urls = ["https://www.transfermarkt.de/spielbericht/index/spielbericht/4095985"]
@@ -10,8 +10,4 @@ class TransfermarktspiderSpider(scrapy.Spider):
         spieler = response.css('span.aufstellung-rueckennummer-name')
         endstand = response.css('div.sb-endstand::text').get()
 
-
-        for player in spieler:
-            yield {
-                'player_link': player.css('a::attr(href)').get(),
-            }
+        yield {'endstand': endstand}
