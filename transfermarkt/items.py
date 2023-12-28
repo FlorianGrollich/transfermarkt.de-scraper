f0220e6518f3690ea55d0ad9e565971fac3c4fc8
FlorianGrollich <F-Grollich@web.de>
2023-12-28 22:52:26 +0100

    feat: add scrapy item for matchitem

--- transfermarkt/items.py
+++ transfermarkt/items.py
@@ -1,12 +1,26 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class TransfermarktItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class MatchItem(scrapy.Item):
+    startingTeam1 = scrapy.Field()
+    benchTeam1 = scrapy.Field()
+    coachTeam1 = scrapy.Field()
+    endScoreTeam1 = scrapy.Field()
+    halfScoreTeam1 = scrapy.Field()
+    team1 = scrapy.Field()
+    #placementTeam1 = scrapy.Field()
+
+    startingTeam2 = scrapy.Field()
+    benchTeam2 = scrapy.Field()
+    coachTeam2 = scrapy.Field()
+    endScoreTeam2 = scrapy.Field()
+    halfScoreTeam2 = scrapy.Field()
+    team2 = scrapy.Field()
+    #placementTeam2 = scrapy.Field()
+
+    referee = scrapy.Field()
+    stadium = scrapy.Field()
+    amountOfViewers = scrapy.Field()
+    date = scrapy.Field()
+    #temperature = scrapy.Field()
+    competition = scrapy.Field()
