d29190fda6604d3b6790e181bdbb294a3e464ac6
FlorianGrollich <F-Grollich@web.de>
2023-12-29 15:09:07 +0100

    feat: add mongodb to pipeline

--- transfermarkt/pipelines.py
+++ transfermarkt/pipelines.py
@@ -1,13 +1,21 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+class TransfermarktPipeline(object):
+    def __init__(self):
+        self.client = None
+        self.db = None
+        self.collection = None
 
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient('localhost', 27017)
+        self.db = self.client['transfermarkt_db']  # Database name
+        self.collection = self.db['matches']  # Collection name
+        self.collection.insert_one({"test": "connection"})
 
-class TransfermarktPipeline:
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.collection.insert_one(dict(item))
         return item
